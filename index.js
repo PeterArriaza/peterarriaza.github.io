deddcebe6d36c79989f8dc37ebccf24378c99b68
Peter Arriaza <PArriaza@javan-walter.com>
2019-09-20 13:50:13 -0400

    added sticky nav bar

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 'use strict';
 
 window.addEventListener("load", () => {
- document.querySelector("#introWords").classList.add("loaded"); 
+    document.querySelector("#introWords").classList.add("loaded");
 });
+
+window.onscroll = function () {
+    makeNavStick()
+};
+
+// Get the navbar
+var navbar = document.getElementById("navBar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function makeNavStick() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
